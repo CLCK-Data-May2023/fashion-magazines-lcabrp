8fd6ff2da4029ba504bdc110dd0b353034960caf
Leonardo Abreu <lcabreup@gmail.com>
2023-10-12 19:55:56 -0400

    First try

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,1 +1,14 @@
 -- Add your sql here
+
+SELECT
+    cust.customer_name AS   [Customer]
+    , printf("$%.2f", SUM(subs.price_per_month * subs.subscription_length)) AS [Amount Due]
+FROM
+    orders o
+    JOIN customers cust ON cust.customer_id = o.customer_id
+    JOIN subscriptions subs ON subs.subscription_id = o.subscription_id
+WHERE
+    o.order_status = 'unpaid'
+    AND subs.description = 'Fashion Magazine'
+GROUP BY
+    cust.customer_name;
